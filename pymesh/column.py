20c9bd38f9f7166f2929634803b62a32d7e26cad
Jayghosh Rao <j.rao@fz-juelich.de>
2021-07-13 21:25:08 +0200

    Clean column.py
    Remove old/commented/test/trial functions

--- pymesh/column.py
+++ pymesh/column.py
@@ -36,12 +36,17 @@ class Column:
         }
 
         if periodic:
-            # self.operate_periodic(container, packedBed, copy)
-            self.operate_periodic_2(container, packedBed, copy)
+            self.operate_periodic(container, packedBed, copy)
+        else:
+            self.fragment(packedBed.asDimTags(), container.asDimTags(), copyObject=copy, removeObject=True, removeTool=True, cleanFragments=True)
 
-        self.fragment(container, packedBed, copy)
 
-    def operate_periodic_2(self, container, packedBed, copy=False):
+    def operate_periodic(self, container, packedBed, copy=False):
+        """
+        Given a periodic packing and container, create a periodic mesh.
+
+        Note: Periodic packings aren't directly compatible for meshing. They need additional preprocessing.
+        """
         factory = gmsh.model.occ
         factory.synchronize()
 
@@ -123,177 +128,6 @@ class Column:
         allbeads = packedBed.asDimTags() + copied_beads
 
         self.fragment(allbeads, container.asDimTags(), removeObject=True, removeTool=True, copyObject=copy, cleanFragments=True)
-
-
-        import sys; sys.exit() ;
-
-    def operate_periodic(self, container, packedBed, copy=False):
-        """
-        Given a periodic packing and container, create a periodic mesh.
-
-        Note: Periodic packings aren't directly compatible for meshing. They need additional preprocessing.
-        """
-        factory = gmsh.model.occ
-
-        factory.synchronize()
-
-        container_copy = container.asDimTags()
-        container_faces = gmsh.model.getBoundary(container_copy, combined=False, oriented=False)
-
-        for e in container_faces:
-            x,y,z = factory.getCenterOfMass(e[0], e[1])
-            factory.dilate([e], x,y,z, 3,3,3)
-
-        ## TEST
-        allbeads = packedBed.asDimTags()
-        fragmented, fmap = factory.fragment(allbeads, container_faces, removeObject=False, removeTool=False)
-        # fragmented, fmap = factory.fragment(allbeads, container_boundaries)
-        print(len(allbeads))
-
-        fragmented_3D=[]
-        for e in fragmented:
-            if e[0] == 2:
-                factory.remove([e], True)
-            else:
-                fragmented_3D.append(e)
-
-
-        print(len(fragmented))
-        print(len(fragmented_3D))
-
-        uncut_beads = []
-        cut_beads = []
-        cut_beads_new_dt = []
-
-        print("before/after fragment relations:")
-        for e,f in zip(allbeads + container_faces, fmap):
-            # print("parent " + str(e) + " -> child " + str(f))
-            print("parent " + str(e) + " -> " + str(len(f)))
-            if len(f) > 1 and e[0] == 3:
-                cut_beads.append(e)
-                # cut_beads_new_dt.extend(e[1])
-                factory.remove(f, True)
-            elif len(f) == 1:
-                uncut_beads.append(e)
-
-        print(gmsh.GMSH_API_VERSION)
-        import sys; sys.exit()
-
-        # print(cut_beads_new_dt)
-        stacked = stackPeriodic(cut_beads, 'xyz', 4, 4, 4)
-
-        fragmented, fmap = factory.fragment(stacked + uncut_beads, container.asDimTags())
-
-        all = factory.getEntities(dim=3)
-        for e in all:
-            if e not in fmap[-1]:
-                factory.remove([e], recursive=True)
-
-        # for e, f in zip(stacked + uncut_beads, fmap[:-1]):
-        #     # print("parent " + str(e) + " -> child " + str(f))
-        #     if len(f) == 1 and f[0] not in uncut_beads:
-        #         factory.remove(f, recursive=True)
-        #     elif len(f) > 1:
-        #         print(f)
-        #         factory.remove(f, recursive=True)
-
-        # import sys; sys.exit()
-
-        factory.remove(container_faces, recursive=True)
-        factory.synchronize()
-
-        ent = gmsh.model.getEntities(0)
-        gmsh.model.mesh.setSize(ent, 0.2)
-        gmsh.model.mesh.generate(2)
-        gmsh.write("dilatedboundaries.vtk")
-
-
-        # print(cut_beads)
-        # print(len(cut_beads))
-
-
-
-        import sys; sys.exit()
-
-        container_faces = container_faces[5:]
-
-        for plane in container_faces:
-
-            allbeads = packedBed.asCopyDimTags()
-            fragmented, _ = factory.fragment(allbeads, [plane])
-
-            ## remove fragmented cutting planes
-            to_delete = [e for e in fragmented if e[0] == 2]
-            factory.remove(to_delete, recursive=True)
-            cut_beads = [e for e in fragmented if e not in to_delete]
-
-            # cut_beads = fragmented
-
-
-            ## remove unused cutting planes
-            # factory.remove(container_boundaries, recursive=True)
-
-            factory.synchronize()
-
-            ## filter beads
-            points = gmsh.model.getBoundary([plane], False, False, True)
-            # coord = gmsh.model.getValue(points[0][0], points[0][1], [])
-            factory.synchronize()
-            coord = []
-            for point in points:
-                coord.extend(gmsh.model.getValue(point[0], point[1], []))
-            pcoord = gmsh.model.getParametrization(plane[0], plane[1], coord)
-            normals = gmsh.model.getNormal(plane[1], pcoord)
-            n = np.reshape(normals, (len(points), 3))
-
-            if not (n == n[0]).all():
-                raise(ValueError)
-
-            print(">> NORMAL:", n[0])
-            filtered_beads = filter_volumes_with_normal(cut_beads, n[0])
-            print("Beads filtered: ", len(filtered_beads))
-
-            # to_delete = list(set(cut_beads).difference(set(filtered_beads)))
-            # for e in to_delete:
-            #     factory.remove([e], recursive=True)
-            # remaining = [ e for e in cut_beads if e not in to_delete ]
-
-        import sys; sys.exit()
-
-        # factory.remove(container_boundaries, recursive=True)
-        factory.remove(container_copy)
-        # factory.remove(container.asDimTags(), recursive=True)
-        factory.remove(packedBed.asDimTags(), recursive=True)
-        # factory.removeAllDuplicates()
-        # factory.healShapes(sewFaces=False, makeSolids=False)
-
-        # ent = factory.getEntities(dim=3)
-        # for e in ent:
-        #     coord = factory.getCenterOfMass(e[0], e[1])
-        #     if gmsh.model.isInside(container_copy[0][0], container_copy[0][1], coord, parametric=False):
-        #         factory.remove([e], recursive=True)
-
-
-        # ---
-
-        # factory.remove(packedBed.asDimTags(), recursive=True)
-        # factory.remove(container.asDimTags(), recursive=True)
-
-
-        # for e in container_boundaries:
-        #     allbeads = factory.fragment(allbeads,[e])
-
-
-        factory.synchronize()
-
-        # print(container_boundaries)
-        ent = gmsh.model.getEntities()
-        gmsh.model.mesh.setSize(ent, 0.15)
-        gmsh.model.mesh.generate(2)
-        gmsh.write("dilatedboundaries.vtk")
-
-        import sys
-        sys.exit(0)
 
     def fragment(self, object, tool, copyObject=False, copyTool=False, removeObject=False, removeTool=False, cleanFragments=False, cleanAll=False):
         """
