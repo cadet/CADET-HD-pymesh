21a20cca35661096acd7a77f3b76c2bae74bd5a7
Jayghosh Rao <j.rao@fz-juelich.de>
2021-07-14 14:15:23 +0200

    Update bounding box calculation settings for GMSH 4.9.0

--- pymesh/configHandler.py
+++ pymesh/configHandler.py
@@ -12,20 +12,20 @@ def load_config(fname):
 def set_gmsh_defaults(config):
     gmsh.option.setNumber('General.Terminal', 1)
 
-    # gmsh.option.setNumber("Print.GeoLabels", 1) #Default = 1
-    # gmsh.option.setNumber('Print.GeoOnlyPhysicals', 1)
-
     # 1: MeshAdapt | 2: Auto | 5: Delaunay | 6: Frontal | 7: BAMG | 8: DelQuad
     gmsh.option.setNumber("Mesh.Algorithm", deep_get(config, 'mesh.algorithm', 5)) # Default = 2
     # 1: Delaunay | 4: Frontal | 5: Frontal Delaunay | 6: Frontal Hex | 7: MMG3D | 9: RTree | 10: HXT
     gmsh.option.setNumber("Mesh.Algorithm3D", deep_get(config, 'mesh.algorithm3D', 10)) # Default = 1
 
-    ## Improved bounding box calculations
-    ## Can slow things down
-    gmsh.option.setNumber("Geometry.OCCBoundsUseStl"  , 1)
-    gmsh.option.setNumber("Geometry.OCCFixDegenerated"  , 1)
-    gmsh.option.setNumber("Geometry.OCCFixSmallEdges"  , 1)
-    gmsh.option.setNumber("Geometry.OCCFixSmallFaces"  , 1)
-    # gmsh.option.setNumber("Mesh.StlAngularDeflection" , 0.08 )
-    # gmsh.option.setNumber("Mesh.StlLinearDeflection"  , 0.0005)
+    if int(gmsh.GMSH_API_VERSION_MAJOR) == 4:
+        if int(gmsh.GMSH_API_VERSION_MINOR) >= 9:
+            ## Improved bounding box calculations
+            ## Can slow things down
+            gmsh.option.setNumber("Geometry.OCCBoundsUseStl"  , 1)
+            gmsh.option.setNumber("Mesh.StlAngularDeflection" , 0.08 )
+            gmsh.option.setNumber("Mesh.StlLinearDeflection"  , 0.0005)
+
+    # gmsh.option.setNumber("Geometry.OCCFixDegenerated"  , 1)
+    # gmsh.option.setNumber("Geometry.OCCFixSmallEdges"  , 1)
+    # gmsh.option.setNumber("Geometry.OCCFixSmallFaces"  , 1)
     # gmsh.option.setNumber("Mesh.MeshSizeFromCurvature"  , 1)
