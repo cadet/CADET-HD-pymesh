ad527511433b43a03d5fc3abc983673c49c19dd8
Jayghosh Rao <j.rao@fz-juelich.de>
2021-07-19 00:32:05 +0200

    Update logger class to have static string variables

--- pymesh/log.py
+++ pymesh/log.py
@@ -6,27 +6,57 @@ from rich import print as rprint
 from rich.text import Text
 
 class Logger:
+    logout = ""
+    logerr = ""
 
-    def __init__(self):
-        self.logout = ""
-        self.logerr = ""
-        rprint("Logger Initialized")
+    def __init__(self, level=0):
+        self.level = level
 
     def out(self, *message, style=None):
-        msg = " ".join(message)
-        self.logout = "\n".join([self.logout, msg])
+        """
+        Write to stdout
+        """
+        prepend = "".join([' ']*self.level*2) + '|--> '
+        msg = prepend + " ".join(message)
+        Logger.logout = "".join([Logger.logout, msg, '\n'])
         rprint(Text(msg, style=style))
 
     def err(self, *message):
+        """
+        Write to stderr
+        """
         msg = " ".join(message)
-        self.logerr = "\n".join([self.logerr, 'ERROR: ' + msg])
+        Logger.logerr = "".join([Logger.logerr, 'ERROR: ' + msg, '\n'])
         rprint(Text("ERROR: " + msg, style="bold red"))
 
     def warn(self, *message):
+        """
+        Write to stderr
+        """
         msg = " ".join(message)
-        self.logerr = "\n".join([self.logerr, 'WARN: ' + msg])
+        Logger.logerr = "".join([Logger.logerr, 'WARN: ' + msg, '\n'])
         rprint(Text("WARN: " + msg, style="bold yellow"))
 
+    def note(self, *message):
+        """
+        Write to stderr
+        """
+        msg = " ".join(message)
+        Logger.logerr = "".join([Logger.logerr, 'NOTE: ' + msg, '\n'])
+        rprint(Text("NOTE: " + msg, style="bold magenta"))
+
     def die(self, *message, exception=RuntimeError):
+        """
+        Write to stderr, and die
+        """
         self.err(*message)
         raise(exception)
+
+    def write(self, fname):
+        """
+        write to files
+        """
+        with open(fname + '.stdout.log', 'w') as outfile:
+            outfile.write(self.logout)
+        with open(fname + '.stderr.log', 'w') as errfile:
+            errfile.write(self.logerr)
