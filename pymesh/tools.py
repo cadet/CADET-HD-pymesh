6d805677cd6f09fa28b608f9461737a8e66333a4
Jayghosh Rao <j.rao@fz-juelich.de>
2021-07-22 12:44:35 +0200

    Update filter_volumes_with_normal()
    although it's a dangling function (unused)

--- pymesh/tools.py
+++ pymesh/tools.py
@@ -49,10 +49,6 @@ def get_volume_normals(entities):
 def filter_volumes_with_normal(entities, ref_normal):
     """
     Given a list of 3D entities, and reference normal, return a list of entities with surfaces with normals pointing in the reference normal direction.
-
-    NOTE: Unfortunately seems broken. For some reason, gmsh normals aren't that predictable after performing fragmentation with 2D surfaces.
-    In my case, beads cut by the z+ plane didn't have a normal pointing in the z+ direction, even though every other direction followed the pattern.
-    It might eventually be fixed upstream.
     """
     gmsh.model.occ.synchronize()
 
@@ -61,38 +57,13 @@ def filter_volumes_with_normal(entities, ref_normal):
     for e in entities:
         boundaries = gmsh.model.getBoundary([e], False, False, False)
 
-        for surface in boundaries:
-            points = gmsh.model.getBoundary([surface], False, False, True)
+        normals = [get_surface_normal_inner(surface) for surface in boundaries]
 
-            for point in points:
-                # coord.extend(gmsh.model.getValue(point[0], point[1], []))
-                coord = gmsh.model.getValue(point[0], point[1], [])
+        for n in normals:
+            if np.allclose(n, ref_normal):
+                output_entities.append(e)
+                break;
 
-                # pCoord = gmsh.model.getParametrization(surface[0], surface[1], coord)
-                try:
-                    pCoord = gmsh.model.getParametrization(surface[0], surface[1], coord)
-                except:
-                    print("Some error with getting parametrization.")
-                    continue
-
-                curv = gmsh.model.getCurvature(surface[0], surface[1], pCoord)
-
-                ## Don't bother with non-zero curvature surfaces
-                if any(curv):
-                    break
-
-                normals = gmsh.model.getNormal(surface[1], pCoord)
-
-                # print("Surface: {tag}\nCurv: {curv}\nNormals:{normals}\nRefN:{ref}".format(tag=e[1], curv=curv, normals=normals, ref=np.tile(ref_normals, len(curv))))
-
-                # if np.array_equals(normals, np.tile(ref_normals,len(curv))):
-                if np.allclose(normals, np.tile(ref_normal,len(curv))):
-                    output_entities.append(e)
-                    # print("MATCH")
-                    break
-                # print("---")
-
-    print("Finished Filtering")
     return output_entities
 
 
