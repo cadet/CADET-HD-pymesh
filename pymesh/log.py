070b6a015cba89c0b0df48480e57bc615059be66
Jayghosh Rao <j.rao@fz-juelich.de>
2021-07-29 09:55:52 +0200

    Rename Logger.log{out,err} to log_{out,err}_all

--- pymesh/log.py
+++ pymesh/log.py
@@ -5,6 +5,7 @@ Log class for pymesh
 try:
     from rich import print as rprint
     from rich.text import Text
+    # from rich.console import Console
 except ImportError:
     def rprint(obj):
         print(obj)
@@ -14,18 +15,27 @@ except ImportError:
 import datetime
 
 class Logger:
-    logout = []
-    logerr = []
+    log_out_all = []
+    log_err_all = []
     timestamp = "." + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
     default_out_style = "bold green"
-    glyph_head = '❯❯'
-    glyph_tail = ' '
+    # console = Console()
+
+    # glyph_head = '❯❯'
+    glyph_head = '❯'
+    glyph_tail = '❯'
     glyph_end = ''
     # glyph_end  = '❮❮'
     # glyph_head  = '❮❮'
     # glyph_tail = ' '
     # glyph_end = '❯❯'
-    indent_factor = 2
+    indent_factor = 1
+
+    # glyph_head = ''
+    # glyph_tail = ' '
+    # glyph_end = ''
+
+    # gleft = ''
 
     # glyph_head = '|-->'
     # glyph_tail = ' '
@@ -42,7 +52,7 @@ class Logger:
         """
         Default print (without Text wrapper) to be able to print dicts and other stuff
         """
-        Logger.logout.extend([str(i) for i in message])
+        Logger.log_out_all.extend([str(i) for i in message])
         rprint(*message)
 
     def out(self, *message, style=None):
@@ -50,8 +60,8 @@ class Logger:
         Write to stdout
         """
         prepend = "".join([Logger.glyph_tail]*self.level*Logger.indent_factor) + Logger.glyph_head + ' '
-        msg = prepend + " ".join(message) + ' ' + Logger.glyph_end
-        Logger.logout.append(msg)
+        msg = "INFO" + prepend + " ".join(message) + ' ' + Logger.glyph_end
+        Logger.log_out_all.append(msg)
         rprint(Text(msg, style=style or Logger.default_out_style))
 
     def err(self, *message):
@@ -59,7 +69,7 @@ class Logger:
         Write to stderr
         """
         msg = " ".join(message)
-        Logger.logerr.append('ERROR: ' + msg)
+        Logger.log_err_all.append('ERROR: ' + msg)
         rprint(Text("ERROR: " + msg, style="bold red"))
 
     def warn(self, *message):
@@ -67,7 +77,7 @@ class Logger:
         Write to stderr
         """
         msg = " ".join(message)
-        Logger.logerr.append('WARN: ' + msg)
+        Logger.log_err_all.append('WARN: ' + msg)
         rprint(Text("WARN: " + msg, style="bold yellow"))
 
     def note(self, *message):
@@ -75,7 +85,7 @@ class Logger:
         Write to stderr
         """
         msg = " ".join(message)
-        Logger.logerr.append('NOTE: ' + msg)
+        Logger.log_err_all.append('NOTE: ' + msg)
         rprint(Text("NOTE: " + msg, style="bold magenta"))
 
     def die(self, *message, exception=RuntimeError):
@@ -91,6 +101,6 @@ class Logger:
         """
         ts = Logger.timestamp if timestamp else ''
         with open(fname + ts + '.stdout.log', 'w') as outfile:
-            outfile.write("\n".join(self.logout))
+            outfile.write("\n".join(self.log_out_all))
         with open(fname + ts + '.stderr.log', 'w') as errfile:
-            errfile.write("\n".join(self.logerr))
+            errfile.write("\n".join(self.log_err_all))
