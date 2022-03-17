561380a725c798dfc5b441bc0156a130c00fd5c2
Jayghosh Rao <j.rao@fz-juelich.de>
2022-03-17 14:08:11 +0100

    Update git_version() to handle builds in non-git directories

--- setup.py
+++ setup.py
@@ -14,16 +14,19 @@ from setuptools import find_packages, setup, Command
 def git_version():
     """ Return version with local version identifier. """
     import git
-    repo = git.Repo('.git')
-    repo.git.status()
-    print(repo)
-    sha = repo.head.commit.hexsha
-    sha = repo.git.rev_parse(sha, short=6)
-    print(sha)
-    if repo.is_dirty():
-        return '{sha}.dirty'.format(sha=sha)
-    else:
-        return sha
+    try: 
+        repo = git.Repo('.git')
+        repo.git.status()
+        print(repo)
+        sha = repo.head.commit.hexsha
+        sha = repo.git.rev_parse(sha, short=6)
+        print(sha)
+        if repo.is_dirty():
+            return '{sha}.dirty'.format(sha=sha)
+        else:
+            return sha
+    except: 
+        return "0.1"
 
 
 # Package meta-data.
