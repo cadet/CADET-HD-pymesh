70c182ace0000759228265d9888517314f7bba8f
Jayghosh Rao <j.rao@fz-juelich.de>
2022-09-23 17:40:10 +0200

    update flake and lock

--- flake.nix
+++ flake.nix
@@ -1,15 +1,91 @@
 {
-    inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
-    inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/3eb07eeafb52bcbf02ce800f032f18d666a9498d"; };
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-    outputs = { self, nixpkgs, flake-utils }:
-        flake-utils.lib.eachDefaultSystem (system:
-                let 
-                    pkgs = nixpkgs.legacyPackages.${system}; 
-                in
-                {
-                    devShell = import ./shell.nix { inherit pkgs; };
-                }
-                );
+  outputs = { self, nixpkgs, flake-utils }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let 
+    pkgs = nixpkgs.legacyPackages.${system}; 
+    # dir = ''/home/jayghoshter/dev/tools/pymesh/'';
 
-}
+    ## GMSH override to build and install lib files
+    ## Can also be placed in ~/.nixpkgs/config.nix
+    gmsh_with_libs = pkgs.gmsh.overrideAttrs (oldAttrs: rec {
+
+      version = "4.11.0-2ac03e";
+
+      src = pkgs.fetchgit {
+        url = "https://gitlab.onelab.info/gmsh/gmsh";
+        rev = "2ac03e26721ff5ffe20759ef4ad474da6cbf4b44";
+        sha256 = "nwYodyHJIzy5W6FX/D+zfEoNBPQFI0rF6bk/en/3RCQ=";
+      };
+
+      patches = [
+        ./custom_mesh_copy.patch
+      ];
+
+      cmakeFlags = [
+        "-DCMAKE_BUILD_TYPE=Debug"
+        "-DENABLE_BUILD_LIB=1"
+        "-DENABLE_BUILD_SHARED=1"
+        "-DENABLE_BUILD_DYNAMIC=1"
+      ];
+    });
+
+      in 
+      {
+
+        defaultPackage = pkgs.python3Packages.buildPythonPackage{
+          pname = "pymesh";
+          version = "0.1";
+
+          src = ./.;
+
+          propagatedBuildInputs = with pkgs; [
+            python39
+            python3Packages.numpy
+            python3Packages.rich
+            python3Packages.ruamel-yaml
+            python3Packages.GitPython
+            python3Packages.setuptools
+            gmsh_with_libs
+          ];
+
+          doCheck = false;
+        };
+
+
+        devShell = pkgs.mkShell rec {
+          name = "pymesh";
+
+          # buildInputs = [
+          #   # pymesh
+          # ];
+
+          propagatedBuildInputs = with pkgs; [
+            python39
+            python3Packages.numpy
+            python3Packages.rich
+            python3Packages.ruamel-yaml
+            python3Packages.GitPython
+            python3Packages.setuptools
+            gmsh_with_libs
+          ];
+
+          shellHook = ''
+            # Tells pip to put packages into $PIP_PREFIX instead of the usual locations.
+            # See https://pip.pypa.io/en/stable/user_guide/#environment-variables.
+            export PIP_PREFIX=$(pwd)/_build/pip_packages
+            export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+            export PYTHONPATH="${gmsh_with_libs}/lib:$PYTHONPATH"
+            export PYTHONPATH="$(pwd):$PYTHONPATH"
+            export PATH="$PIP_PREFIX/bin:$PATH"
+            export PATH="$(pwd)/bin:$PATH"
+            unset SOURCE_DATE_EPOCH
+            '';
+
+          };
+        }
+        );
+
+      }
