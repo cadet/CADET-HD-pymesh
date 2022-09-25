b7931dcf20e5a9123cff0b929c05db301ad73c7e
Jayghosh Rao <j.rao@fz-juelich.de>
2022-09-25 22:46:57 +0200

    fix git version printing

--- pymesh/__init__.py
+++ pymesh/__init__.py
@@ -5,8 +5,25 @@ A mesher for 3D chromatography columns.
 """
 
 import pkg_resources
+import git
 
-__version__ = pkg_resources.get_distribution("pymesh").version
+def git_version():
+    """ Return version with local version identifier. """
+    try: 
+        repo = git.Repo('.', search_parent_directories=True)
+        repo.git.status()
+        sha = repo.head.commit.hexsha
+        sha = repo.git.rev_parse(sha, short=6)
+        if repo.is_dirty():
+            return '{sha}.dirty'.format(sha=sha)
+        else:
+            return sha
+    except git.InvalidGitRepositoryError: 
+        return None
+
+__version__ = "0.1"
+# If run locally, return the actual git version, otherwise, return the version installed.
+__git_version__ = git_version() or pkg_resources.get_distribution("chromoo").version
 __author__ = 'Jayghosh Rao'
 __credits__ = 'FZJ/IBG-1/ModSim'
 
